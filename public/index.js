c6a8f4412875639bc58a702f62b7f2cb78395be4
Henry W <henry.woodmansee@gmail.com>
2022-11-23 18:24:29 -0500

    pretty much done

--- public/index.js
+++ public/index.js
@@ -1,30 +1,29 @@
-import { setCatPic, resetScore, removeCatPic, vote } from "./utilities.js"
-
-// const scoreCounter = document.querySelector("#scoreCounter")
+import { setCatPic, resetScore, removeCatPic, vote } from "./utilities.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-    setCatPic()
-    resetScore()
+	setCatPic();
+	resetScore();
 
-    const newCatButton = document.querySelector('#newCatButton')
-    newCatButton.addEventListener("click", () => {
-        removeCatPic()
-        setCatPic()
-        resetScore()
-    })
+	const newCatButton = document.querySelector("#newCatButton");
+	newCatButton.addEventListener("click", () => {
+		removeCatPic();
+		setCatPic();
+		resetScore();
+	});
 
-    const upVoteButton = document.querySelector("#upVoteButton")
-    upVoteButton.addEventListener("click", () => {
-        vote("upVote")
-    })
+	const upVoteButton = document.querySelector("#upVoteButton");
+	upVoteButton.addEventListener("click", () => {
+		vote("upVote");
+	});
 
-    const downVoteButton = document.querySelector("#downVoteButton")
-    downVoteButton.addEventListener("click", () => {
-        vote("downVote")
-    })
+	const downVoteButton = document.querySelector("#downVoteButton");
+	downVoteButton.addEventListener("click", () => {
+		vote("downVote");
+	});
 
-})
-
-/// onload vs DOMContentLoaded
-/// Should we put all our functions in our load function?
-/// Why does this work as it is? Wasn't working outside the function
+	const commentButton = document.querySelector("#comment-submit-button");
+	commentButton.addEventListener("click", event => {
+		event.preventDefault();
+		addComment();
+	});
+});
--- public/utilities.js
+++ public/utilities.js
@@ -1,35 +1,66 @@
 const setCatPic = async () => {
-    let request = await fetch("https://api.thecatapi.com/v1/images/search")
-    const res = await request.json()
-    const catUrl = res[0].url
+	let request = await fetch("https://api.thecatapi.com/v1/images/search");
+	const res = await request.json();
+	const catUrl = res[0].url;
 
-    const catImage = document.createElement("img")
-    catImage.src = catUrl
-    const imgContainer = document.querySelector(".image-container");
-    imgContainer.appendChild(catImage)
-}
+	const catImage = document.createElement("img");
+	catImage.src = catUrl;
+	const imgContainer = document.querySelector(".image-container");
+	imgContainer.appendChild(catImage);
+};
 
 const removeCatPic = () => {
-    const catImage = document.querySelector("img")
-    catImage.remove()
-}
+	const catImage = document.querySelector("img");
+	catImage.remove();
+};
 
 function resetScore() {
-    const scoreCounter = document.querySelector("#scoreCounter")
-    let score = Number(scoreCounter.innerText)
-    score = 0;
-    console.log(scoreCounter)
-    scoreCounter.innerText = score
+	const scoreCounter = document.querySelector("#scoreCounter");
+	let score = Number(scoreCounter.innerText);
+	score = 0;
+	scoreCounter.innerText = score;
 }
 
 function vote(type) {
-    const scoreCounter = document.querySelector("#scoreCounter")
-    let score = Number(scoreCounter.innerText)
-    if (type === "upVote") score++
-    if (type === "downVote") score--
-    scoreCounter.innerText = score
+	const scoreCounter = document.querySelector("#scoreCounter");
+	let score = Number(scoreCounter.innerText);
+	if (type === "upVote") score++;
+	if (type === "downVote") score--;
+	scoreCounter.innerText = score;
+	if (score > 0) scoreCounter.className = "positive-score";
+	if (score < 0) scoreCounter.className = "negative-score";
+	if (score === 0) scoreCounter.className = "zero-score";
+	return score;
 }
 
+function addComment() {
+	const commentInputField = document.querySelector("#addCommentInput");
+	let text = commentInputField.value;
+	if (text) {
+		const commentContainer = document.querySelector(
+			".comment-display-container"
+		);
+		// comment box
+		const individualCommentBox = document.createElement("div");
+		individualCommentBox.className = "comment-box";
+		commentContainer.append(individualCommentBox);
+		//comment
+		const comment = document.createElement("p");
+		comment.className = "comment-text";
+		comment.innerText = text;
+		individualCommentBox.append(comment);
 
+		//deletbutton
+		const deleteButton = document.createElement("button");
+		deleteButton.className = "delete-button";
+		deleteButton.innerText = "X";
+		individualCommentBox.append(deleteButton);
+		deleteButton.addEventListener("click", () => {
+			individualCommentBox.remove();
+		});
+	}
+	commentInputField.value = "";
+	return individualCommentBox;
+}
 
-export { setCatPic, resetScore, removeCatPic, vote }
+export { setCatPic, resetScore, removeCatPic, vote, addComment };
